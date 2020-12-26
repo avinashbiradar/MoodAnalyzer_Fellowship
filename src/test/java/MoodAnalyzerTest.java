4e535503ee6d4f59b2ce8903f13c7a3cf2aeef6c
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-26 22:33:23 +0530

    Refactor : use case 1 is refactored to custom exception

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,14 +1,23 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenNullMoodShouldReturnHappy(){
+    public void givenNullMoodShouldReturnHappy()  {
         MoodAnalyzer moodAnalyzer=new MoodAnalyzer(null);
-        String mood = moodAnalyzer.analysemood();
-    Assert.assertEquals("HAPPY",mood);
+        String mood = null;
+        try {
+            ExpectedException exceptionRule =ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyzer.analysemood();
+            Assert.assertEquals("HAPPY",mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
